9af51d29ef4cf03296a0af4c51ce54e90ceb4c84
malkhasyan <ara.malkhasyan2019@gmail.com>
2022-07-29 16:46:44 +0400

    Create functions for cell generation:   find_empty_cell()   generate_number()   add_cell()

--- terminal_game.py
+++ terminal_game.py
@@ -31,3 +31,62 @@ def print_matrix(arr):
     for i in range(SIZE):
         print(floor, end='')
     print()
+
+# Generating cells
+
+def find_empty_cells(arr):
+    """
+    Returns a list of tuples with empty cell indices: (i, j).
+
+    Parameters:
+         arr(list): the matrix
+    Returns:
+        empty_cells(list): list of indices of empty cells
+    """
+    empty_cells = []
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] == 0:
+                empty_cells.append((i, j))
+    return empty_cells
+
+def generate_number():
+    """
+    Randomly returns 2 or 4
+
+    Paramters:
+        pass
+    Returns:
+        value(int): random number
+    """
+    value = 2 if random.random() <= 0.72 else 4
+    return value
+
+def add_cell(arr):
+    """
+    Generates new cell
+
+    Paramters:
+        arr(list): the matrix
+    Returns:
+         None
+    """
+    num = generate_number()
+    empty_cells = find_empty_cells(arr)
+    try:
+        cell = random.choice(empty_cells)
+        arr[cell[0]][cell[1]] = num
+    except IndexError:
+        gameover()
+
+# Game status
+
+def gameover():
+    print('*** GameOver ***')
+    print(f'Your Score: {SCORE}')
+
+def is_gameover():
+    pass
+
+def is_won():
+    pass
